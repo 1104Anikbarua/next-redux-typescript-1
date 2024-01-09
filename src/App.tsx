72353c0cfa5a46e359cfea930f7c58b2959255cb
Anik Barua <anikbb05@gmail.com>
2024-01-09 22:57:31 +0600

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return <></>;
+}
+
+export default App;
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/1104Anikbarua/next-redux-typescript-1.git
+git push -u origin main
